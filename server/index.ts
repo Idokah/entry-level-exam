b1f05f4257eb53954d54220a30898ac28c60f7be
Ido <idokk96@gmail.com>
2021-03-04 09:15:54 +0200

    show content: show more-show less, search query
    need to improve show more,show less (that all the containers will be in the same size at the begging)

--- server/index.ts
+++ server/index.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import bodyParser = require('body-parser');
 import { tempData } from './temp-data';
 import { serverAPIPort, APIPath } from '@fed-exam/config';
+import { url } from 'inspector';
 
 console.log('starting server', { serverAPIPort, APIPath });
 
@@ -22,11 +23,27 @@ app.get(APIPath, (req, res) => {
 
   // @ts-ignore
   const page: number = req.query.page || 1;
-
+  const search: string = req.query.search as string|| "";
   const paginatedData = tempData.slice((page - 1) * PAGE_SIZE, page * PAGE_SIZE);
+  
 
+  if (search!="") {
+  const filteredTickets = tempData//paginatedData
+  .filter((t) => (t.title.toLowerCase() + t.content.toLowerCase()).includes(search.toLowerCase()));
+  //
+  //const result=filteredTickets.slice(0,20);  //get top 20 results.
+  //
+
+  // res.send (filteredTickets);
+  res.send (filteredTickets);
+  }
+  else
+  {
   res.send(paginatedData);
+  }
 });
+
+
 
 app.listen(serverAPIPort);
 console.log('server running', serverAPIPort)
